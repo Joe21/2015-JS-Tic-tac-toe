1fc883a313241f0670ee96c4b12892ee756010c6
Joe <joej21@gmail.com>
2015-06-10 18:58:13 -0400

    rescoped game, need to get down asych control flow for new games

--- js/scripts.js
+++ js/scripts.js
@@ -1,95 +1,127 @@
+// On document ready, instantiate globalContainer
 $(document).ready(function() {
-	ticTacToe();
+	globalContainer();
 });
 
 // ==========================================================================
-// MASTER CONTROLLER
+// MASTER CONTROLLER aka globalContainer
 // ==========================================================================
+// Constructor for global container
+// 	- This design allows for persistent scorekeeping with newer games
+// 	- Deters javascript hacking to manipulate the score
 
-// Constructor function for the application aka Master Controller for the app
-function ticTacToe() {
-	var gameMode, player1, player2;
-	gameMenu(player1, player2);
+function globalContainer() {
+	var globalSettings = new globalSettingsObject;
+	var globalPlayer1 = new player(1);
+	var globalPlayer2 = new player(2);
+
+	// event driven gameMenu with callbacks to handle initializers
+	gameMenu(globalSettings, globalPlayer1, globalPlayer2);
+
+	// if(globalSettings.gameOver === true) {
+	// 	startGame(globalSettings, globalPlayer1, globalPlayer2);
+	// }
 
 
-}
+	// ==========================================================================
+	// GLOBAL CONTAINER OBJECTS
+	// ==========================================================================
 
-// ==========================================================================
-// INITIALIZE GAME SETTINGS
-// ==========================================================================
-
-// Constructor function to prototype player object
-function player(id) {
-	this.id = id,
-	this.name = "",
-	this.wins = 0,
-	this.losses = 0,
-	this.draws = 0,
-	this.avatar = ""
-}
-
-// Constructor function to prototype game object
-function gameObject() {
-	this.player1 = null,
-	this.player2 = null,
-	this.turn = null,
-	this.gameOver = false
-}
-
-// --------------------------------------------------------------------------
-
-// Event driven game menu with callbacks to render and start game
-function gameMenu(player1, player2) {
-	$('#pve-button').click(function() {
-		gameMode = 'pve';
-		initializeScoreboard(player1, player2);
-		toggleWrapper();
-	})
-
-	$('#pvp-button').click(function() {
-		gameMode = 'pvp';
-		initializeScoreboard(player1, player2);
-		toggleWrapper();
-		startPVPGame(player1, player2);
-	})
-}
-
-// Helper function for gameMenu
-// Passing the player1 & player2 arguments allows us to manage scope and event handler within tictactoe()
-function initializeScoreboard(player1, player2) {
-	// initialize for 1 player vs AI
-	if(gameMode == 'pve') {
-		player1 = new player(1);
-		player1.name = prompt('Please enter name for player 1');
-		$('#player1-name').text(player1.name);
-		
-		player2 = new player(2);
-		var namesArray = ['Evil Robot', 'T-1000', 'Cylon', 'Megatron', 'SkyNet'];
-		player2.name = namesArray[Math.floor(Math.random()*namesArray.length)];
-		$('#player2-name').text(player2.name);
-
-	} else {
-	// initialize for 2 players head to head
-		player1 = new player(1);
-		player1.name = prompt('Please enter name for player 1');
-		$('#player1-name').text(player1.name);
-
-		player2 = new player(2);
-		player2.name = prompt('Please enter name for player 2');
-		$('#player2-name').text(player2.name);
+	// Constructor function to prototype player object
+	function player(id) {
+		this.id = id,
+		this.name = "",
+		this.wins = 0,
+		this.losses = 0,
+		this.draws = 0,
+		this.avatar = ""
 	}
-}
 
-// Close overlay and render wrapper
-function toggleWrapper(){
-	var wrapper	= $('#wrapper');
-	var overlay = $('#overlay');
-	wrapper.toggle();
-	overlay.toggle();
-}
+	// Constructor function to prototype settings object
+	function globalSettingsObject() {
+		this.gameMode = "",
+		this.gameOver = null;
+	}
 
-// --------------------------------------------------------------------------
+	// ==========================================================================
+	// INITIALIZE SETTINGS
+	// ==========================================================================
 
+	function gameMenu(globalSettings, globalPlayer1, globalPlayer2) {
+		$('#pve-button').click(function() {
+			globalSettings.gameMode = 'pve';
+			
+			// initialize scoreboard, set player names
+			initializeScoreboard(globalSettings, globalPlayer1, globalPlayer2);
+
+			// Closes overlay
+			toggleWrapper();
+
+			globalSettings.gameOver = true;
+
+		})
+
+		$('#pvp-button').click(function() {
+			globalSettings.gameMode = 'pvp';
+
+			// initialize scoreboard, set player names
+			initializeScoreboard(globalSettings, globalPlayer1, globalPlayer2);
+
+			// Closes overlay
+			toggleWrapper();
+
+			globalSettings.gameOver = true;
+		})
+		return;
+	}
+
+	function initializeScoreboard(globalSettings, globalPlayer1, globalPlayer2) {
+		// initialize for 1 player vs AI
+		if(globalSettings.gameMode == 'pve') {
+			globalPlayer1.name = prompt('Please enter name for player 1');
+			$('#player1-name').text(globalPlayer1.name);
+			
+			var namesArray = ['Evil Robot', 'T-1000', 'Cylon', 'Megatron', 'SkyNet'];
+			globalPlayer2.name = namesArray[Math.floor(Math.random()*namesArray.length)];
+			$('#player2-name').text(globalPlayer2.name);
+
+		} else {
+		// initialize for 2 players head to head
+			globalPlayer1.name = prompt('Please enter name for player 1');
+			$('#player1-name').text(globalPlayer1.name);
+
+			globalPlayer2.name = prompt('Please enter name for player 2');
+			$('#player2-name').text(globalPlayer2.name);
+		}
+	}
+
+	// Close overlay and render wrapper
+	function toggleWrapper(){
+		var wrapper	= $('#wrapper');
+		var overlay = $('#overlay');
+		wrapper.toggle();
+		overlay.toggle();
+	}
+
+
+	// ==========================================================================
+	// INITIALIZE GAME
+	// ==========================================================================
+	function startGame(globalSettings, globalPlayer1, globalPlayer2) {
+		var game;
+		globalSettings.gameOver = false;
+
+		// if(globalSettings.gameMode == 'pve') {
+		// 	game = new pveGame(globalSettings, globalPlayer1, globalPlayer2);
+		// } else {
+		// 	game = new pvpGame(globalSettings, globalPlayer1, globalPlayer2);
+		// }
+
+		console.log('started a new game')
+		return;
+	}
+
+} // <---- End of Global Container
 
 // ==========================================================================
 // GAME ENGINE
@@ -137,6 +169,16 @@ function MakeMove() {
 }
 
 
+
+
+
+// Reqs
+// - Switch in globalcontainer to tell if gameOver
+// - If gameOver, ask to play again
+
+
+
+// --------
 // - Game variables
 // • turn
 // • over?
